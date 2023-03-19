b36200b63013567c14e6e9be8ab5828c52c638d7
cindytyas <cindysyaira21@gmail.com>
2023-03-19 21:22:26 +0700

    Initial commit

---
+++ page/home.php
@@ -0,0 +1,7 @@
+<div class="container-fluid text-white p-5" style="background-color: #423342;">
+    <div class="container p-5" style="background-color: #423342;">
+        <h1>Selamat Datang, <?= $user ?>! </h1>
+        <p>Selamat datang di aplikasi inventaris kantor</p>
+        <p>Apliksi ini merupakan aplikasi untuk uji level XI RPL 1</p>
+    </div>
+</div>
